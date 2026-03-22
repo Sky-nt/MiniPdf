48fc9e19a343e62ed02052827c9c66b76b116992
Wei Lin <shps951002@gmail.com>
2026-03-22 17:39:40 +0800

    Add startup script and font installation for Azure deployment

--- MiniPdf.Api/Program.cs
+++ MiniPdf.Api/Program.cs
@@ -1,5 +1,17 @@
 using MiniSoftware;
 
+// Register fonts from /home/fonts/ (Azure App Service persistent storage)
+// and from <app>/Fonts/ (local development fallback)
+foreach (var dir in new[] { "/home/fonts", Path.Combine(AppContext.BaseDirectory, "Fonts") })
+{
+    if (!Directory.Exists(dir)) continue;
+    foreach (var file in Directory.GetFiles(dir, "*.ttf"))
+    {
+        var name = Path.GetFileNameWithoutExtension(file);
+        MiniPdf.RegisterFont(name, File.ReadAllBytes(file));
+    }
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
